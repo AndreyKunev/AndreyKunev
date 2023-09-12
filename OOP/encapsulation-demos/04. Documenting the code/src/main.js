0600d728b94109154aacde2dd378a1674ed17c20
Andrey Kunev <akunev45@gmail.com>
2023-09-12 20:51:54 +0300

    removing again

--- OOP/encapsulation-demos/04. Documenting the code/src/main.js
+++
@@ -1,9 +0,0 @@
-import { Person } from './person.js';
-
-const personOne = new Person('Ivan', 'Ivanov', 9010101234);
-personOne.sayIntroduction();
-
-personOne.firstName = 'Pesho'; // This calls the set firstName property
-personOne.lastName = 'Peshov'; // This calls the set lastName property
-
-console.log(personOne.fullName); // This calls the get fullName property
