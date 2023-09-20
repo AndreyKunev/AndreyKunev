8d122e8f70c4d5ad81c6c1c96fb3eaac428c501b
Andrey Kunev <akunev45@gmail.com>
2023-09-20 20:33:53 +0300

    finished contact class

--- OOP/OOP-additional-Phonebook/Phonebook/Template/src/models/contact.js
+++ OOP/OOP-additional-Phonebook/Phonebook/Template/src/models/contact.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 export class Contact {
   static #minNameLength = 3;
   static #maxNameLength = 25;
+  static #maxCallLength = 150;
   #name;
   #phone;
   #history;
@@ -14,9 +15,16 @@ export class Contact {
       throw new Error(`Name length should be between ${Contact.#minNameLength} and ${Contact.#maxNameLength}`);
     }
 
-    this.phone = phone;
+    this.#name = name;
+    this.setPhone(phone);
+    this.#history = [];
   }
 
+  /**
+   * Checks if the input is valid
+   * @param {any} value
+   * @throws {Error} if input is not valid
+   */
   invalidStringCheck(value) {
     if (!value || !(typeof value === 'string')) {
       throw new Error('Input is not valid!');
@@ -24,31 +32,92 @@ export class Contact {
 
   }
 
+  /**
+   * Get contact phone number
+   * @return {string} phone number
+   */
   get phone() {
     return this.#phone;
   }
 
-  set phone(phone) {
+  /**
+   * Get contact name
+   * @return {string} name
+   */
+  get name() {
+    return this.#name;
+  }
+
+  /**
+   * Gets history
+   * @return {Array} history
+  */
+  get history() {
+    return [...this.#history];
+  }
+
+  /**
+   * Sets phone number, performs validation
+   * @param {string} phone - phone number to set for contact
+   * @throws {Error} if phone number isn't valid
+   */
+  setPhone(phone) {
     this.invalidStringCheck(phone);
+
+    // we use two regular expressions as we can use either spaces or hyphens, not a mix of both
     const regexHyphen = /^0\d{3}-\d{3}-\d{3}$/;
     const regexSpace = /^0\d{3} \d{3} \d{3}$/;
 
-    if (!(regexHyphen.test(phone)) || !(regexSpace.test(phone))) {
+    if (!(regexHyphen.test(phone) || regexSpace.test(phone))) {
       throw new Error('Invalid phone format - use only spaces or only hyphens!');
     }
 
     this.#phone = phone;
   }
 
+  /**
+   * Logs call to history with time of the call and random duration
+   */
   call() {
-    // missing implementation
+    const timeOfCall = moment().format('M/D/YYYY h:mm:ss A');
+    const callDuration = Math.ceil(Math.random() * Contact.#maxCallLength);
+    const callInLogFormat = `[${timeOfCall}]Call: Duration: ${callDuration} sec.`;
+
+    this.#history.push(callInLogFormat);
+
   }
 
+  /**
+   * Validates if message is in valid format and logs to history
+   * with time of message
+   * @param {string} msg
+   */
   message(msg) {
-    // missing implementation
+    this.invalidStringCheck(msg);
+
+    const timeOfMessage = moment().format('M/D/YYYY h:mm:ss A');
+    const messageInLogFormat = `[${timeOfMessage}]Message: (${msg})`;
+
+    this.#history.push(messageInLogFormat);
+
   }
 
+  /**
+   * Logs history from newest to oldest
+   * Logs No entries if history is empty
+   */
   viewHistoryLog() {
-    // missing implementation
+    if (this.history.length === 0) {
+      console.log('No entries');
+    } else {
+      let logString = `${this.name}'s call log\n`;
+      for (let i = this.history.length - 1; i >= 0; i--) {
+        const element = this.history[i];
+        logString += `${element}\n`;
+      }
+
+      console.log(logString);
+    }
+
   }
 }
